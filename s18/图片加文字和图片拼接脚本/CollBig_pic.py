5a1f3bfca7cdfd0bd22c873bc09fcda6675a6e3b
yahooj2020 <greenbirch2007@yahoo.co.jp>
2020-08-13 13:19:24 +0000

    add pic coll

---
+++ s18/图片加文字和图片拼接脚本/CollBig_pic.py
@@ -0,0 +1,34 @@
+
+import PIL.Image as Image
+
+import os
+import math
+pathList = []
+headImgPath= 'D:\Python3.8\pp\\'
+ # os.listdir() 方法用于返回指定的文件夹包含的文件或文件夹的名字的列表。这个列表以字母顺序。 它不包括 '.' 和'..' 即使它在文件夹中。
+for item in os.listdir(headImgPath):
+    imgPath = os.path.join(headImgPath, item)
+    pathList.append(imgPath)
+ 
+total = len(pathList)#total是好友头像图片总数
+line = int(math.sqrt(total))#line是拼接图片的行数（即每一行包含的图片数量）
+NewImage = Image.new('RGB', (128*line,128*line))
+x = y = 0
+for item in pathList:
+
+    try:
+        img = Image.open(item)
+       # Image.ANTIALIAS：平滑滤波。对所有可以影响输出像素的输入像素进行高质量的重采样滤波，以计算输出像素值。
+        img = img.resize((128,128),Image.ANTIALIAS)
+        NewImage.paste(img, (x * 128 , y * 128))
+        x += 1
+    except IOError:
+        print("第%d行,%d列文件读取失败！IOError:%s" % (y,x,item))
+        x -= 1
+    if x == line:
+
+        x = 0
+        y += 1
+    if (x+line*y) == line*line:
+        break
+NewImage.save("final.jpg")
